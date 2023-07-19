9849260de070878757374324e450200b59fdd79d
Manny Vera <vallesvera.manuel@gmail.com>
2023-07-18 21:41:51 -0700

    Footers

--- js/starter.js
+++ js/starter.js
@@ -4,26 +4,35 @@ $(document).ready(function () {
   $window.on("scroll", function () {
     let pixels = $window.scrollTop();
     console.log(`This is the pixel distance: ${pixels}`);
-    let bar = document.getElementById("navbar");
+    let navBarEl = $("#navbar");
 
     if (pixels > 130) {
-      bar.setAttribute(
-        "class",
-        "navbar navbar-expand-lg navbar-dark bg-dark sticky-top navbar-custom"
-      );
+      navBarEl
+        .removeClass("navbar-light bg-light ")
+        .addClass("navbar-dark bg-dark navbar-custom");
     } else {
-      bar.setAttribute(
-        "class",
-        "navbar navbar-expand-lg navbar-light bg-light sticky-top"
-      );
+      navBarEl
+        .removeClass("navbar-dark bg-dark navbar-custom")
+        .addClass("navbar-light bg-light");
     }
   });
 
+  // Obtains month for the newsletter dropdown option
   let dateData = new Date();
   let monthNumber = dateData.getMonth();
-  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  $("#newsMonth").text(`${month[monthNumber]} News Letter`)
-
-//   let observer = new MutationObserver(function(){})
-
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  $("#newsMonth").text(`${month[monthNumber]} News Letter`);
 });
